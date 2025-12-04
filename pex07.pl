e0ec5a26372f3b3e92fb143ba8fd68024018999b
markalbertalano <135277040+markalbertalano@users.noreply.github.com>
2025-12-03 18:26:38 -0700

    almost final submission
    Added author name and documentation details.

--- pex07.pl
+++ pex07.pl
@@ -1,12 +1,11 @@
 % pex5.pl
 % USAFA UFO Sightings 2024
 %
-% name: 
+% name: mark alano
 %
-% Documentation: 
+% Documentation: used hw as a template ; searched up how to implement or statements on stack overflow
 %
 
 % The query to get the answer(s) or that there is no answer
 % ?- solve.
-
 
